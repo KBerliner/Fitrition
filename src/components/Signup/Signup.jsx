03afeaa3e81cfa2e03437f124aaeec0dcbc9169b
Kyle <berlinerkyle@gmail.com>
2024-02-11 12:38:56 -0700

    Added the basic Signup functionality. A user can sign up with a username, email, and password. The password is hashed prior to transportation to backend.

--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Signup.module.css';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUserId, signup } from '../../features/users/usersSlice';
+
 
 export default function Login() {
+    const dispatch = useDispatch();
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        console.log(e.target);
+        const user = {
+            username,
+            email,
+            password
+        }
+        
+        dispatch(signup(user));
     }
 
     return (
@@ -15,11 +29,11 @@ export default function Login() {
             <form onSubmit={handleSubmit}>
                 <h1>Sign Up Here</h1>
                 <label htmlFor="username">Username</label>
-                <input name="username" type="text" required></input>
+                <input onChange={({ target }) => setUsername(target.value)} name="username" type="text" value={username} required></input>
                 <label htmlFor="email">Email</label>
-                <input name="email" type="email" required></input>
+                <input onChange={({ target }) => setEmail(target.value)} name="email" type="email" value={email} required></input>
                 <label htmlFor="password">Password</label>
-                <input name="password" type="password" required></input>
+                <input onChange={({ target }) => setPassword(target.value)} name="password" type="password" value={password} required></input>
                 <div>
                     <input name="remember" type="checkbox"></input>
                     <label htmlFor="remember">Remember Me</label>
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,4 +1,39 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import { hash } from 'bcryptjs';
+
+export const signup = createAsyncThunk(
+    'users/signup',
+    async (reqBody) => {
+        return new Promise((resolve, reject) => {
+            let request = new XMLHttpRequest();
+    
+            hash(reqBody.password, 10).then(
+                hash => {
+                    const user = {
+                        "username": reqBody.username,
+                        "email": reqBody.email,
+                        "password": hash
+                    }
+    
+                    request.open('POST', `${import.meta.env.VITE_API_URL}signup`);
+                    request.setRequestHeader('Content-type', 'application/json');
+                    request.send(JSON.stringify(user));
+                }
+            )
+    
+            request.onreadystatechange = () => {
+                if (request.readyState === 4) {
+                    if (request.status === 200 || request.status === 201) {
+                        resolve(JSON.parse(request.response));
+                    } else {
+                        reject(JSON.parse(request.response));
+                    }
+                }
+            }
+        })
+    }
+)
 
 export const usersSlice = createSlice({
     name: "user",
@@ -6,6 +41,32 @@ export const usersSlice = createSlice({
         user: {
             username: "",
             userId: ""
-        }
+        },
+        isLoading: false,
+        hasError: false
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(signup.fulfilled, (state, action) => {
+            state.hasError = false;
+            state.isLoading = false;
+            state.user.username = action.payload.username;
+            state.user.userId = action.payload.userId;
+            if (localStorage) {
+                localStorage.setItem('token', action.payload.token)
+            }
+            })
+            .addCase(signup.pending, (state, action) => {
+            state.hasError = false;
+            state.isLoading = true;
+            })
+            .addCase(signup.rejected, (state, action) => {
+            state.hasError = true;
+            state.isLoading = false;
+        })
     }
-})
+});
+
+export const { selectUserId } = (state) => state.user.userId;
+
+export default usersSlice.reducer;
