41f87cb2fe06b6fb964c4580502325872a81d9db
Kyle <berlinerkyle@gmail.com>
2024-02-19 11:07:41 -0700

    Added the check for a user that isn't logged in on the log workout component.

--- src/components/WorkoutLog/WorkoutLog.jsx
+++ src/components/WorkoutLog/WorkoutLog.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./WorkoutLog.module.css";
+import { Navigate } from "react-router";
+import Login from "../Login/Login";
 
 export default function WorkoutLog() {
+	const token = localStorage ? localStorage.getItem("token") : undefined;
+
 	return (
 		<>
-			<form>
-				<label for="type">Type</label>
-				<select name="type"></select>
-			</form>
+			{token ? (
+				<form>
+					<label for="type">Type</label>
+					<select name="type"></select>
+				</form>
+			) : (
+				<Navigate to={<Login />} />
+			)}
 		</>
 	);
 }
