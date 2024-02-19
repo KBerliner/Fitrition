3167e7c3a3b92e3d1dda9ca277cdd62816ce2e39
Kyle <berlinerkyle@gmail.com>
2024-02-19 16:25:28 -0700

    Added all of the extraReducers functions to handle the thunks.

--- src/features/workouts/workoutsSlice.js
+++ src/features/workouts/workoutsSlice.js
@@ -96,38 +96,6 @@ export const deleteWorkout = createAsyncThunk(
 	}
 );
 
-// Exporting the Get Workouts of Type thunk
-
-export const allWorkoutsOfType = createAsyncThunk(
-	"workouts/allOfType",
-	async (reqBody) => {
-		return new Promise((resolve, reject) => {
-			// Defining new XMLHttpRequest
-			let request = new XMLHttpRequest();
-
-			request.open(
-				"GET",
-				`${import.meta.env.VITE_API_URL}workout/all/${reqBody.type}`
-			);
-			request.setRequestHeader("Content-type", "application/json");
-			request.setRequestHeader(
-				"Authorization",
-				`Bearer ${useSelector((state) => state.users.user.token)}`
-			);
-
-			request.onreadystatechange = () => {
-				if (request.readyState === 4) {
-					if (request.status === 200 || 201) {
-						resolve(JSON.parse(request.response));
-					} else {
-						reject(JSON.parse(request.response));
-					}
-				}
-			};
-		});
-	}
-);
-
 // Exporting the Get All Workouts thunk
 
 export const allWorkouts = createAsyncThunk("workouts/all", async (reqBody) => {
@@ -167,10 +135,11 @@ export const workoutsSlice = createSlice({
 	extraReducers: (builder) => {
 		builder
 
-			// Handling Fullfilled Workout Add API request
+			// Handling Fulfilled Workout Add API request
 			.addCase(addWorkout.fulfilled, (state, action) => {
 				state.hasError = false;
 				state.isLoading = false;
+				// Update the state with the new workout
 				state.workouts.workouts.push(action.payload);
 			})
 
@@ -182,6 +151,71 @@ export const workoutsSlice = createSlice({
 
 			// Handling Rejected Workout API request
 			.addCase(addWorkout.rejected, (state, action) => {
+				state.hasError = true;
+				state.isLoading = false;
+			})
+
+			// Handling Fullfilled Change Workout API request
+			.addCase(changeWorkout.fulfilled, (state, action) => {
+				state.hasError = false;
+				state.isLoading = false;
+				// Update the state with the modified workout
+				const updatedWorkoutIndex = state.workouts.findIndex(
+					(workout) => workout._id.toString() === action.payload.id
+				);
+				state.workouts[updatedWorkoutIndex] = action.payload;
+			})
+
+			// Handling Pending Change Workout API request
+			.addCase(changeWorkout.pending, (state, action) => {
+				state.hasError = false;
+				state.isLoading = true;
+			})
+
+			// Handling Rejected Change Workout API request
+			.addCase(changeWorkout.rejected, (state, action) => {
+				state.hasError = true;
+				state.isLoading = false;
+			})
+
+			// Handling Fullfilled Delete Workout API request
+			.addCase(deleteWorkout.fulfilled, (state, action) => {
+				state.hasError = false;
+				state.isLoading = false;
+				// Remove the deleted workout from the state
+				state.workouts = state.workouts.filter(
+					(workout) => workout._id.toString() !== action.payload.id
+				);
+			})
+
+			// Handling Pending Delete Workout API request
+			.addCase(deleteWorkout.pending, (state, action) => {
+				state.hasError = false;
+				state.isLoading = true;
+			})
+
+			// Handling Rejected Delete Workout API request
+			.addCase(deleteWorkout.rejected, (state, action) => {
+				state.hasError = true;
+				state.isLoading = false;
+			})
+
+			// Handling Fullfilled Get All Workouts API request
+			.addCase(allWorkouts.fulfilled, (state, action) => {
+				state.hasError = false;
+				state.isLoading = false;
+				// Update the state with the fetched workouts
+				state.workouts = action.payload;
+			})
+
+			// Handling Pending Get All Workouts API request
+			.addCase(allWorkouts.pending, (state, action) => {
+				state.hasError = false;
+				state.isLoading = true;
+			})
+
+			// Handling Rejected Get All Workouts API request
+			.addCase(allWorkouts.rejected, (state, action) => {
 				state.hasError = true;
 				state.isLoading = false;
 			});
