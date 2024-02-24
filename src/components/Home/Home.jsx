5b114310ddde109daa52b234af0b9048e77682d0
Kyle <berlinerkyle@gmail.com>
2024-02-24 11:50:51 -0700

    Added Loading status for when the workout dashboard is loading.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,19 +13,45 @@ export default function Home({ type }) {
 	const menuItems = useSelector((state) => state.users.user.workouts);
 
 	const workoutHistory = useSelector((state) => state.workouts.workouts);
+	const loadingWorkouts = useSelector((state) => state.workouts.isLoading);
+
+	const renderGraphs = () => {
+		for (let i = 0; i < menuItems.length; i++) {
+			return (
+				<LineGraph
+					chartData={workoutHistory.filter(
+						(workout) => workout.type === menuItems[i]
+					)}
+				/>
+			);
+		}
+	};
 
 	return (
 		<>
 			{!expired ? (
-				<div>
+				<div className={styles.home_container}>
 					<Header type={type} menuItems={menuItems} />
-					{workoutHistory.length > 0 ? (
-						<div className={styles.graph_grid_container}>
-							<LineGraph chartType="general" chartData={workoutHistory[0]} />
-						</div>
-					) : (
-						<h2 className={styles.no_workouts}>No workouts logged yet!</h2>
-					)}
+					<div className={styles.graph_grid_container}>
+						{workoutHistory.length > 0 && !loadingWorkouts ? (
+							menuItems.map((menuItem, index) => {
+								if (index < 4) {
+									return (
+										<LineGraph
+											key={index}
+											chartData={workoutHistory.filter(
+												(workout) => workout.type === menuItems[index]
+											)}
+										/>
+									);
+								}
+							})
+						) : !loadingWorkouts ? (
+							<h2 className={styles.no_workouts}>No workouts logged yet!</h2>
+						) : (
+							<h2>Loading...</h2>
+						)}
+					</div>
 					<Footer sendTo={type} />
 				</div>
 			) : (
--- src/components/LineGraph/LineGraph.jsx
+++ src/components/LineGraph/LineGraph.jsx
@@ -1,26 +1,28 @@
 import React from "react";
-import { AreaChart, Area, XAxis, ResponsiveContainer, YAxis } from "recharts";
+import {
+	AreaChart,
+	Area,
+	XAxis,
+	ResponsiveContainer,
+	YAxis,
+	Tooltip,
+} from "recharts";
 import styles from "./LineGraph.module.css";
 
 export default function LineGraph({ chartData, chartType }) {
-	console.log(chartData);
-
 	return (
 		<div className={styles.chart_container}>
-			<h1>{chartType}</h1>
+			{/* <h1>{chartType}</h1> */}
 			<ResponsiveContainer width="100%" height="100%">
-				<AreaChart width={500} height={500} data={chartData}>
+				<AreaChart data={chartData}>
+					<YAxis />
+					<XAxis />
+					<Tooltip />
 					<Area
 						type="monotone"
-						dataKey="totalLbs"
+						dataKey="calBurned"
 						stroke="#5ba896"
 						fill="#66b3a1"
-					/>
-					<Area
-						type="monotone"
-						stroke="#5ba89a"
-						fill="#66b3aa"
-						dataKey="totalReps"
 					/>
 					<XAxis dataKey="totalLbs" />
 				</AreaChart>
